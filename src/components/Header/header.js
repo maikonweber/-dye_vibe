26450dc1d5866943838054cf9efaac9039f49e72
Maikon <maikonweber@gmail.com>
2021-08-03 22:22:56 -0300

    Adicionado Header Styles

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -14,18 +14,39 @@ const Header = () => {
         <Link to='#' className='menu'>
             <Falicons.FaBars className='icons-menu' onClick={showSidebar} />
             </Link> 
+            <div className='legenda'> @DyeVibe </div>
             <Link to='#' className="shop-bag">
-             <Falicons.FaShoppingBag className='shop-bag'/>
-             </Link>
-             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-             <ul className='nac-menu-items'>
+            <Falicons.FaShoppingBag className='shop-bag'/>
+            </Link>
+            <Link to='#' className='user-icon' >
+            <Falicons.FaUserCircle className="user-icon"/>
+            </Link>
+            <nav className={sidebar ? 'nav-menu-active' : 'nav-menu'}>
+            <ul className='nav-menu-items'>
                 <li className='nav-menu-items'>
-                    <Link to='#' className='menu-bars' >
-                        <Falicons.FaTimes className='x-icon' > </Falicons.FaTimes>
-                    </Link>
-                    <Link to='#' className='user' >
-                        <Falicons.FaUserCircle className="user-icon"></Falicons.FaUserCircle>
-                    </Link>
+                <Link to='#' className='menu-bars' >
+                <Falicons.FaTimes className='x-icon' />
+                </Link>            
+                </li>
+                <li>
+                <Link to='#' className='menu-item'> Home
+                <Falicons.FaStore className='menu-home'/> 
+                </Link>
+                </li>
+                <li>
+                <Link to='#' className='menu-item'> Newsletter
+                <Falicons.FaStore className='menu-new'/> 
+                </Link>
+                </li>
+                <li>
+                <Link to='#' className='menu-item'> Produtos
+                <Falicons.FaStore className='menu-produtos'/> 
+                </Link>
+                </li>
+                <li>
+                <Link to='#' className='menu-item'> Perfil
+                <Falicons.FaStore className='menu-perfil'/>
+                </Link>
                 </li>
              </ul>      
             </nav>   
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,14 +5,24 @@ export const Container = styled.div`
     grid-template-columns: repeat(10, 3fr);
     height: 60px;
     background: #312F2F;
-    width: 100vw;    
+    width: 100vw;
+
+    .legenda {
+        color: red;
+        font-size: 35px;
+        grid-column: 5/6;
+        position: relative;
+        top: 5px;
+        left: 15px;
+    }
 
     .icons-menu {
+        grid-column: 1/2;
         font-size: 35px;
-        position: relative;
-        left: 10px;
-        top: 10px;
         color: #85FFFF;
+        position: relative;
+        top: 10px;
+        left: 10px;
     }
 
     .shop-bag {
@@ -23,7 +33,57 @@ export const Container = styled.div`
         font-size : 35px;
         color: #85FFFF;
     }
+    .user-icon {
+        grid-column: 10/10;
+        font-size: 35px;
+        color: #85FFFF;
+        position: relative;
+        left: 15px;
+        top: 6px;
+    }
 
+    .nav-menu {
+        justify-content:start;
+        align-items: center;
+       
+    }
+
+    .nav-menu-active {
+        height: 100vw;
+        background: #312F2F;
+        width: 20vw;
+    }
+
+    .menu-item {
+        font-size: 20px;
+        position: relative;
+        top: 30px;
+        color: #85FFFF;
+        left: 13px;
+    }
+
+    .menu-home {
+        position: relative;
+        color: #85FFFF;
+        left: 20px;
+    }
+
+    .menu-new {
+        position: relative;
+        left:110px;
+        top: -20px;
+    }
+
+    .menu-produtos {
+        position: relative;
+        left: 100px;
+        top: -20px;
+    }
+
+    .menu-pefil {
+        position: relative;
+        left: 20px;
+    }
 
     `
 ;
