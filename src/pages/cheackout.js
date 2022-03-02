47820314526064bbbcf152e57f51441942bd1784
unknown <Maikon Weber>
2022-03-02 01:01:41 -0300

    dye vibe

--- src/pages/cheackout.js
+++ src/pages/cheackout.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState, useEffect} from 'react';
-import { Flex, Input, Button, Stack, FormLabel, FormControl, Box , Grid} from '@chakra-ui/react'
+import { Flex, Input, Button, Stack, FormLabel, FormControl, Divider ,Box , Grid, Image, Td, Thead, Tbody, Tr, Tfoot, Th , Table} from '@chakra-ui/react'
 import { AuthContext } from '../context/AuthContext';
 
 
@@ -11,18 +11,56 @@ const {cart, handleAddtoCart, handleRemoveItemFromCart, clearCart} = useContext(
 console.log(cart)
 
 
+
+
     return (
         <>
-            <Flex align='center' justify='center' h='100vh'>
-                <Grid templateColumns='repeat(3, 1fr)' gap={8}>
-                    <Stack spacing={4} />
-                    {cart.map((item, index) => (
-                        <Box key={index}
-                                        w='150px' h='150px' bgGradient='linear(to-r,red.200, green.200)' p='10px'>
-                    </Box>
-                         ))}
-                                </Grid>
-                                        </Flex>                
+            <Flex align='start' justify='center' position='relative' top={0} h='100vh'>
+                <Box bg='white' minWidth={480} maxWidth={800} borderRadius={6} shadow='md' rounded='lg'>
+                <Table size='md' colorScheme={'green'}>
+  <Thead>
+    <Tr>
+      <Th>Product</Th>
+      <Th> Valor </Th>
+      <Th >Retirar</Th>
+    </Tr>
+  </Thead>
+  <Tbody>
+      {
+    cart.map((item, index) => (
+        <Tr key={index}>
+            <Td><Image src={item.img} alt={item.name} minHeight={160} maxWidth={150} maxHeight={180} minWidth={120} borderRadius={8}/></Td>
+            <Td>R${item.valor}</Td>
+            <Td><Button w={20} color={'red'} bg='white' onClick={clearCart}> Remover </Button></Td>
+        </Tr>
+    ))
+}
+  </Tbody>
+  <Tfoot>
+    <Tr>
+      <Th>To</Th>
+      <Th>into</Th>
+      <Th isNumeric>multiply by</Th>
+    </Tr>
+  </Tfoot>
+</Table>
+<Box >
+    <Box p={5}>
+    <Button onClick={clearCart}>Limpar Carrinho</Button>
+    <Button onClick={clearCart} marginLeft={'45%'}> Finalizar </Button>
+    
+    </Box>
+      <Box p={2}>
+        
+          <Input placeholder='Consultar Cep' />
+          <Button>Buscar</Button>
+      </Box>
+    </Box>     
+
+</Box>
+
+</Flex>  
+
             </>
        
     )
--- src/services/PageServices.js
+++ src/services/PageServices.js
@@ -1,4 +1,7 @@
-export async function queryProductsMainPage() {
+let Correios = require('node-correios');
+let correios = new Correios();
+
+ async function queryProductsMainPage() {
     const response = await fetch(`http://localhost:3092/products`, {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
@@ -6,5 +9,35 @@ export async function queryProductsMainPage() {
 
     return await response.json();
   }
+
   
+  const getCepInformation = async (Cep) => {
+    cep = new CepBrasil('08330500');
+    const response = await cep.consultar();
+    
+    // const response = await fetch(`http://viacep.com.br/ws/01001000/json/`, {
+    //   method: 'GET',
+    //   headers: { 'Content-Type': 'application/json' }
+    // })
+
+    return await response;
+  }
+
+  const getCepInformation2 = async (Cep) => {
+    let args = {
+      nCdServico: '40010,41106,40215',
+      // demais parâmetros ...
+  }
+    correios.calcPreco(args)
+  .then(result => {
+  console.log(result);
+  })
+.catch(error => {
+  console.log(error);
+  });
+
+  }
+
+
+  getCepInformation2('08330500');
   
